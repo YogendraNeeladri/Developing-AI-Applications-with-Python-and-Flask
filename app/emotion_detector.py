5f791202ed38ab4c8211a2dc25f2822b052cae3d
Yogendra Neeladri <115712703+YogendraNeeladri@users.noreply.github.com>
2025-09-05 22:48:17 +0530

    emotion_detector.py

--- app/emotion_detector.py
+++ app/emotion_detector.py
@@ -1,1 +1,27 @@
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+# Replace with your IBM Watson NLU API key & URL
+API_KEY = 'YOUR_API_KEY'
+URL = 'YOUR_URL'
+
+authenticator = IAMAuthenticator(API_KEY)
+nlp = NaturalLanguageUnderstandingV1(
+    version='2023-08-01',
+    authenticator=authenticator
+)
+nlp.set_service_url(URL)
+
+def detect_emotion(text):
+    if not text.strip():
+        return {'error': 'Empty text provided'}, 400
+    try:
+        response = nlp.analyze(
+            text=text,
+            features=Features(emotion=EmotionOptions())
+        ).get_result()
+        return response['emotion']['document']['emotion'], 200
+    except Exception as e:
+        return {'error': str(e)}, 500
 
