04fa3bbcb4ef55d9238f1924b6b0f78ea781b8ab
Yogendra Neeladri <115712703+YogendraNeeladri@users.noreply.github.com>
2025-09-05 22:48:32 +0530

    emotion_predictor.py

--- app/emotion_predictor.py
+++ app/emotion_predictor.py
@@ -1,1 +1,11 @@
+from .emotion_detector import detect_emotion
+
+def emotion_predictor(text):
+    result, status = detect_emotion(text)
+    if status != 200:
+        return result  # error dictionary
+    
+    # Format output
+    formatted = {emotion: f"{round(score*100, 2)}%" for emotion, score in result.items()}
+    return formatted
 
